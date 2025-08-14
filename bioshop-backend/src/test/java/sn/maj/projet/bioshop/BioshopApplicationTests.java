24d00ef1d3942b7ee5f85e67b1d0bcb67f59249f
GruntM <fmadjiguene@ept.sn>
2025-08-14 12:12:48 +0000

    add backend and frontend sources

---
+++ bioshop-backend/src/test/java/sn/maj/projet/bioshop/BioshopApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.maj.projet.bioshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
